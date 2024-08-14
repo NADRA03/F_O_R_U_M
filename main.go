47bbf18e5d62cad29aa9c502da8956757e4cbb16
Malak <malakhabeeb2003@gmail.com>
2024-08-14 19:56:17 +0300

    profile page

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
     http.HandleFunc("/like", Forum.LikeHandler(db))
     http.HandleFunc("/comment", Forum.CommentHandler(db))
     http.HandleFunc("/addpost",Forum.AddPostHandler(db))
+    http.HandleFunc("/profile",Forum.ViewProfileHandler(db))
     fmt.Println("Server started at :8080")
     http.ListenAndServe(":8080", nil)
 }
